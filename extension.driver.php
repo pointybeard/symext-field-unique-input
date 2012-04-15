f0b5bbb1c427cd311887adc678689213488db768
Nick Dunn <nick@nick-dunn.co.uk>
2012-04-15 22:20:32 +0100

    rm meta data from about()

--- extension.driver.php
+++ extension.driver.php
@@ -2,19 +2,6 @@
 
 	Class extension_uniqueinputfield extends Extension{
 
-		public function about(){
-			return array(
-				'name' => 'Field: Unique Text Input',
-				'version' => '1.4',
-				'release-date' => '2011-12-17',
-				'author' => array(
-					'name' => 'Symphony Team',
-					'website' => 'http://www.symphony21.com',
-					'email' => 'team@symphony21.com'
-				)
-			);
-		}
-
 		public function uninstall(){
 			Symphony::Database()->query("DROP TABLE `tbl_fields_uniqueinput`");
 		}
